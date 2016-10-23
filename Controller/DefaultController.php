a4f9bace25b29add6d1d3c5de24b859350407fb0
Simon Mall <simonkmall@gmail.com>
2016-10-23 23:12:16 +0200

    Added the BaseUser class and the create method.

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -3,11 +3,95 @@
 namespace Mallapp\SimpleauthBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Mallapp\SimpleauthBundle\Entity\BaseUser;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+ 
+    
+    public function createAction(Request $request)
+    {
+    
+        $em = $this->getDoctrine()->getManager();
+        $repository = $this->getDoctrine()->getRepository('MallappSimpleauthBundle:BaseUser');
+
+        
+        $params = $this->getRequestBodyJsonParameters($request);
+        
+        if (!array_key_exists('nickname', $params)) {
+            
+            return new JsonResponse(array('status' => 'nok', 'message' => 'NO_NICKNAME'));
+        
+        }
+        
+        $nickname = $params['nickname'];
+        
+        $newUser = new BaseUser();
+        
+        if (array_key_exists('email', $params)) {
+         
+            // Check if already exists
+            
+            $email = $params['email'];
+                    
+            $existingUser = $repository->findOneByEmail($email);
+            
+            if ($existingUser != null) {
+                
+                // Update existing
+                $existingUser->setNickname($nickname);
+        
+                $em->flush();
+                
+                return new JsonResponse(array('status' => 'ok', 'token' => $existingUser->getToken()));
+                
+            }
+            else {
+                
+                // Create new with email
+                $newUser->setEmail($email);
+                
+            }
+            
+        }
+        
+        $newUser->setNickname($nickname);
+        
+        do {
+        
+            $tokenCandidate = BaseUser::createToken();
+        
+        } while ($repository->findOneByToken($tokenCandidate) != null);
+            
+        $newUser->setToken($tokenCandidate);
+        
+        $em->persist($newUser);
+        
+        $em->flush();
+        
+        return new JsonResponse(array('status' => 'ok', 'token' => $newUser->getToken()));
+        
+        
+        
+    }
+    
+    public function resendAction(Request $request)
     {
         return $this->render('MallappSimpleauthBundle:Default:index.html.twig');
+    }
+    
+    private function getRequestBodyJsonParameters(Request $request) {
+        
+        $params = array();
+        $content = $request->getContent();
+        if (!empty($content))
+        {
+            $params = json_decode($content, true);
+        }
+        
+        return $params;
+        
     }
 }
---
+++ Entity/BaseUser.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace Mallapp\SimpleauthBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * BaseUser
+ *
+ * @ORM\Table(name="base_user")
+ * @ORM\Entity(repositoryClass="Mallapp\SimpleauthBundle\Repository\BaseUserRepository")
+ */
+class BaseUser
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="token", type="string", length=255, unique=true)
+     */
+    private $token;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, nullable=true, unique=true)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nickname", type="string", length=255)
+     */
+    private $nickname;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set token
+     *
+     * @param string $token
+     *
+     * @return BaseUser
+     */
+    public function setToken($token)
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+
+    /**
+     * Get token
+     *
+     * @return string
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return BaseUser
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set nickname
+     *
+     * @param string $nickname
+     *
+     * @return BaseUser
+     */
+    public function setNickname($nickname)
+    {
+        $this->nickname = $nickname;
+
+        return $this;
+    }
+
+    /**
+     * Get nickname
+     *
+     * @return string
+     */
+    public function getNickname()
+    {
+        return $this->nickname;
+    }
+
+
+    public static function createToken() {
+        
+        $length = 10;
+        
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        
+        return $randomString;
+        
+    }
+    
+}
+
